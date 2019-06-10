83d47fa9b7a48139207b30f155f8faced1af4f8f
Victor Santana <victor.santana@brandedholdings.com>
2019-06-10 04:54:09 -0400

    Convert Google Doc Images to Gatsby Nodes (#22)

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,22 +1,42 @@
 const {getAuth} = require("./utils/auth")
 const {fetchGoogleDriveFiles} = require("./utils/google-drive")
 const {fetchGoogleDocsDocuments} = require("./utils/google-docs")
+const {createRemoteFileNode} = require("gatsby-source-filesystem");
 
 const DEFAULT_CONFIG = {
   access_type: "offline",
-  redirect_uris: ["urn:ietf:wg:oauth:2.0:oob", "http://localhost"],
-  scope: [
-    "https://www.googleapis.com/auth/documents.readonly",
-    "https://www.googleapis.com/auth/drive.metadata.readonly",
+  redirect_uris: [
+    "urn:ietf:wg:oauth:2.0:oob", "http://localhost"
   ],
-  token_path: "google-docs-token.json",
+  scope: [
+    "https://www.googleapis.com/auth/documents.readonly", "https://www.googleapis.com/auth/drive.metadata.readonly"
+  ],
+  token_path: "google-docs-token.json"
 }
 
-exports.sourceNodes = async (
-  {actions: {createNode}, createNodeId, createContentDigest},
-  {config, ...options},
-  done
-) => {
+async function getGoogleImages(document) {
+  const imageNodeArr = [];
+  for (const element of document.content) {
+    const imgTag = element.img;
+    if (imgTag) {
+      imageNodeArr.push(imgTag);
+    }
+  }
+  return imageNodeArr;
+}
+
+exports.sourceNodes = async ({
+  actions: {
+    createNode
+  },
+  createNodeId,
+  createContentDigest,
+  store,
+  cache
+}, {
+  config,
+  ...options
+}, done) => {
   if (!config.api_key) {
     throw new Error("source-google-docs: Missing API key")
   }
@@ -34,34 +54,56 @@ exports.sourceNodes = async (
   }
 
   try {
-    const auth = await getAuth({...DEFAULT_CONFIG, ...config})
-
-    const googleDriveFiles = await fetchGoogleDriveFiles({
-      auth,
-      rootFolderIds: options.foldersIds,
-      fields: options.fields,
-      fieldsMapper: options.fieldsMapper,
-      fieldsDefault: options.fieldsDefault,
+    const auth = await getAuth({
+      ...DEFAULT_CONFIG,
+      ...config
     })
 
-    const googleDocsDocuments = await fetchGoogleDocsDocuments({
-      auth,
-      apiKey: config.api_key,
-      googleDriveFiles,
-    })
+    const googleDriveFiles = await fetchGoogleDriveFiles({auth, rootFolderIds: options.foldersIds, fields: options.fields, fieldsMapper: options.fieldsMapper, fieldsDefault: options.fieldsDefault})
 
-    googleDocsDocuments.forEach(document => {
-      createNode({
+    const googleDocsDocuments = await fetchGoogleDocsDocuments({auth, apiKey: config.api_key, googleDriveFiles})
+    const convertImgToNode = options.convertImgToNode;
+    let image = [];
+    for (document of googleDocsDocuments) {
+      const id = createNodeId(`GoogleDocs-${document.id}`);
+      let markdownNode = {
         document,
-        id: createNodeId(`GoogleDocs-${document.id}`),
+        id,
         internal: {
           type: "GoogleDocs",
-          mediaType: "text/markdown",
-          content: document.markdown,
-          contentDigest: createContentDigest(document.markdown),
-        },
-      })
-    })
+          mediaType: "text/markdown"
+        }
+      };
+      if (convertImgToNode) {
+        images = await getGoogleImages(document, convertImgToNode, id);
+        for (imgObj of images) {
+          const imageToken = Math.random().toString(36).substr(2, 9);
+          try {
+            const url = imgObj.source;
+            const imageNode = await createRemoteFileNode({
+              url,
+              parentNodeId: id,
+              store,
+              cache,
+              createNode,
+              createNodeId,
+              name: `google-doc-image-${imageToken}`
+            })
+
+            if (imageNode) {
+              document.markdown = document.markdown.replace(url, imageNode.id);
+            } else {
+              return;
+            }
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      }
+      markdownNode.internal.content = document.markdown;
+      markdownNode.internal.contentDigest = createContentDigest(markdownNode);
+      createNode(markdownNode);
+    }
     done()
   } catch (e) {
     done(new Error(`source-google-docs: ${e.message}`))
