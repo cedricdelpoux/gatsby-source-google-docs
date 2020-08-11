5155eaf3a7f5b67f7d7582cf47e16fa58693ea4a
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-08-11 08:42:27 +0200

    Replace GoogleDocs links by relative paths

--- src/utils/google-docs.js
+++ src/utils/google-docs.js
@@ -32,28 +32,46 @@ async function fetchGoogleDocs({id}) {
 
 async function fetchGoogleDocsDocuments(pluginOptions) {
   const googleDriveFiles = await fetchGoogleDriveFiles(pluginOptions)
+  const relativePaths = {}
 
-  return Promise.all(
+  const googleDocsDocuments = await Promise.all(
     googleDriveFiles.map(async file => {
       const {cover, content} = await fetchGoogleDocs({
         id: file.id,
       })
 
-      const markdown = convertJsonToMarkdown({
-        metadata: {...file, cover},
-        content,
-      })
+      relativePaths[file.id] = file.path
 
-      const document = {
-        ...file,
-        cover,
-        content,
-        markdown,
-      }
-
-      return document
+      return {file, cover, content}
     })
   )
+
+  return googleDocsDocuments.map(({file, cover, content}) => {
+    let markdown = convertJsonToMarkdown({
+      metadata: {...file, cover},
+      content,
+    })
+
+    // Replace Google Docs urls by relative paths
+    const googleDocsUrlsMatches = markdown.matchAll(
+      /https:\/\/docs.google.com\/document\/d\/([a-zA-Z0-9_-]+)/g
+    )
+
+    for (const [url, id] of googleDocsUrlsMatches) {
+      if (relativePaths[id]) {
+        markdown = markdown.replace(new RegExp(url, "g"), relativePaths[id])
+      }
+    }
+
+    const document = {
+      ...file,
+      cover,
+      content,
+      markdown,
+    }
+
+    return document
+  })
 }
 
 module.exports = {
