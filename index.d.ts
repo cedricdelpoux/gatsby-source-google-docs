5821656592db1e8e4801ebd13c69b963b5391475
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-12-11 09:00:06 +0100

    docs: update typescript definition file

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,22 @@
 import {drive_v3} from "googleapis"
 
 export interface Options {
+  /**
+   * folder ID can be found in Google Drive URLs
+   * https://drive.google.com/drive/folders/FOLDER_ID
+   */
+  folder: string
+  //
   //---
   // All the following options are OPTIONAL
   //---
+  //
   /**
-   * To fetch only documents to specific folders
-   * folders Ids can be found in Google Drive URLs
-   * https://drive.google.com/drive/folders/FOLDER_ID
+   * To add default fields values
    */
-  folders?: string[]
+  createPages?: boolean
   /** h1 -> h2, h2 -> h3, ... */
   demoteHeadings?: boolean
-  /**
-   * You could need to fetch additional documents fields to your nodes
-   * All available options: https://developers.google.com/drive/api/v3/reference/files#resource
-   */
-  fields?: string[]
   /**
    * To add default fields values
    */
@@ -25,7 +25,7 @@ export interface Options {
    * To ignore some folder in the tree
    * It can be folder names or IDs
    */
-  ignoredFolders?: string[]
+  ignore?: string[]
   /**
    * For a better stack trace and more information
    * Usefull when you open a issue to report a bug
@@ -43,16 +43,15 @@ export interface RawFolder extends drive_v3.Schema$File {
 
 export interface Metadata extends DocumentFile {
   id?: DocumentFile["id"]
-  /** The filename, like path.basename(filepath) */
   name: string
+  slug: string
   path: string
   description?: string | object
-  content: any[]
   cover: {
     image: any
     title: any
     alt: any
   }
   markdown: string
-  breadcrumb: string[]
+  breadcrumb: object[]
 }
