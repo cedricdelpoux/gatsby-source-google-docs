41150cae1ea38be789d98b54adb13fe32c09fb0f
Cédric Delpoux <cedric.delpoux@gmail.com>
2021-11-14 07:11:31 +0100

    feat: remove MarkdownRemark nodes images support

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,7 @@
 const {createPages} = require("./utils/create-pages")
 const {createSchema} = require("./utils/create-schema")
-const {onCreateNode} = require("./utils/on-create-node")
 const {sourceNodes} = require("./utils/source-nodes")
 
 exports.createPages = createPages
 exports.createSchemaCustomization = createSchema
-exports.onCreateNode = onCreateNode
 exports.sourceNodes = sourceNodes
