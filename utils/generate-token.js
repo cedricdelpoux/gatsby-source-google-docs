e70b3950fd0a914d1bf824b4efc8c250cc8174e1
Cédric Delpoux <cedric.delpoux@gmail.com>
2023-03-28 13:20:36 +0200

    fix: change googleapis token scope

--- utils/generate-token.js
+++ utils/generate-token.js
@@ -7,7 +7,7 @@ async function generateToken() {
   const googleOAuth2 = new GoogleOAuth2({
     scope: [
       "https://www.googleapis.com/auth/documents.readonly",
-      "https://www.googleapis.com/auth/drive.metadata.readonly",
+      "https://www.googleapis.com/auth/drive.readonly",
     ],
     token: "GOOGLE_DOCS_TOKEN",
     apis: ["docs.googleapis.com", "drive.googleapis.com"],
